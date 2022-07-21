c5f01225c35c903565fd7554d8a588880b42c01d
nano <4840723+mr-nano@users.noreply.github.com>
2022-07-21 17:05:30 +0000

    build. steps to run mr-sequential

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 export GOPATH=/workspaces/mit-go-mr
+alias gst="git status"
+go env -w GO111MODULE=off
+cd src/main
+rm mr-out*
+go build -race -buildmode=plugin ../mrapps/wc.go
+go run -race mrsequential.go wc.so pg*.txt
+more mr-out-0
